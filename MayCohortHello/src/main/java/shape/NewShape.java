5a52a80c83a2ae2e489b2611ea4e42ddad11205e
Sophia Zhou <102894133+sbagel@users.noreply.github.com>
2023-05-09 10:51:50 -0700

    first program in java

---
+++ MayCohortHello/src/main/java/shape/NewShape.java
@@ -0,0 +1,8 @@
+package shape;
+
+public class NewShape {
+    public static void main(String[] args) {
+        System.out.println("  *  \n * *\n*   *\n * * \n  * \n");
+        System.out.println("hi");
+    }
+}
